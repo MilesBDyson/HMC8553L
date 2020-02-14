883f358a619f06634f1de65ee03c10802839bcca
MilesBDyson <MilesBDyson@gmx.com>
2020-02-14 10:56:05 -0500

    modified for BBB

--- reading.py
+++ reading.py
@@ -1,13 +1,13 @@
-import smbus
-import math
-import time
-import sys
+#!/usr/bin/env python
+
 import hmc5883l
 
 compass = hmc5883l.hmc5883l(gauss = 4.7, declination = (-2,5))
 
 test1 = compass.heading()
 test2 = compass.axes()
+test3 = compass.degrees(compass.heading())
 
-#print test1
-print test2
+print "deg " + str(test1)
+print "axes " + str(test2)
+print "deg/min " + str(test3)
